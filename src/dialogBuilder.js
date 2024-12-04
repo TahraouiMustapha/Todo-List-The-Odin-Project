5f06c05861892742b8f378a1093b7138544c13c9
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2024-12-04 18:46:31 +0100

    Feat : create a module who responsible of crteating dialogs to add new project or task

---
+++ src/dialogBuilder.js
@@ -0,0 +1,212 @@
+import closeIcon from "./assets/close.svg";
+
+const dialogBuilder = (function() {
+ 
+    const createHead = (titleContent) => {
+        const myDiv = document.createElement('div');
+        myDiv.classList.add('head');
+            const myTitle = document.createElement('p');
+            myTitle.classList.add('title');
+            myTitle.textContent = titleContent;
+
+            const closeBtn = document.createElement('a');
+                const img = document.createElement('img');
+                img.src = closeIcon;
+                img.alt = "icon";
+            closeBtn.appendChild(img);
+
+
+        myDiv.appendChild(myTitle);
+        myDiv.appendChild(closeBtn);
+
+        closeBtn.addEventListener('click', () => {
+            const dialog = document.querySelector('dialog[open]');
+            dialog.close();
+        })
+        return myDiv;
+    }
+
+    const createTitleInput = () => {
+        const inputContainer = document.createElement('p');
+            const label = document.createElement('label');
+            label.setAttribute('for', 'title');
+            label.textContent = 'Title';
+
+            const input = document.createElement('input');
+            input.setAttribute('type', 'text');
+            input.setAttribute('id', 'title');
+            input.setAttribute('name', 'todos_title');
+            input.setAttribute('placeholder', 'Title');
+            input.setAttribute('required', 'true');
+
+
+        inputContainer.appendChild(label);    
+        inputContainer.appendChild(input);    
+
+        return inputContainer;
+    }
+
+    const createDescInput = () => {
+        const inputContainer = document.createElement('p');
+            const label = document.createElement('label');
+            label.setAttribute('for', 'desc');
+            label.textContent = 'Description';
+
+            const input = document.createElement('textarea');
+            input.setAttribute('id', 'desc');
+            input.setAttribute('name', 'todos_desc');
+            input.setAttribute('placeholder', 'Description');
+            input.setAttribute('required', 'true');
+
+
+        inputContainer.appendChild(label);    
+        inputContainer.appendChild(input);    
+
+        return inputContainer;        
+    }
+ 
+    const createDueDateInput = () => {
+        const inputContainer = document.createElement('p');
+            const label = document.createElement('label');
+            label.setAttribute('for', 'dueDate');
+            label.textContent = 'Due Date';
+
+            const input = document.createElement('input');
+            input.setAttribute('type', 'date');
+            input.setAttribute('id', 'dueDate');
+            input.setAttribute('name', 'todos_dueDate');
+            input.setAttribute('required', 'true');
+
+
+        inputContainer.appendChild(label);    
+        inputContainer.appendChild(input);    
+
+        return inputContainer;
+    }
+
+    const createOption = (value, txt) => {
+        const opt = document.createElement('option');
+            opt.setAttribute('value', value);
+            opt.textContent = txt;
+        return opt;
+    }
+
+    const createPriorityInput = () => {
+        const inputContainer = document.createElement('p');
+            const label = document.createElement('label');
+            label.setAttribute('for', 'priority');
+            label.textContent = 'Priority';
+
+            const select = document.createElement('select');
+            select.setAttribute('id', 'priority');
+            select.setAttribute('name', 'todos_priority');
+
+            const firstOpt = document.createElement('option');
+            firstOpt.textContent = 'How much important to you?';
+            firstOpt.setAttribute('disbled', 'true');
+            firstOpt.setAttribute('selected', 'true');
+            select.appendChild(firstOpt);
+            select.appendChild(createOption('notImportant', 'Not Important'));
+            select.appendChild(createOption('aBitImportant', 'A bit Important'));
+            select.appendChild(createOption('veryImportant', 'Very Important'));
+
+
+
+
+        inputContainer.appendChild(label);    
+        inputContainer.appendChild(select);    
+
+        return inputContainer;
+    }
+
+    const createAddBtn = () => {
+        const btn = document.createElement('button');
+            btn.setAttribute('id', 'dialog-add-btn');
+            btn.textContent = 'Add';
+        return btn;
+    }
+
+    const createCancelBtn = () => {
+        const btn = document.createElement('button');
+            btn.setAttribute('type', 'button');
+            btn.textContent = 'Cancel';
+        return btn;
+    }
+
+    //functions that appears the dialogs
+    const addNewTodosDialog = () => {
+        const dialog = document.createElement('dialog');
+        const form = document.createElement('form');
+        form.setAttribute('method', 'dialog');
+
+            //create the div of inputs
+            const inputsDiv = document.createElement('div');
+            inputsDiv.classList.add('inputs-div');
+                inputsDiv.appendChild(createTitleInput())
+                inputsDiv.appendChild(createDescInput());
+                inputsDiv.appendChild(createDueDateInput());
+                inputsDiv.appendChild(createPriorityInput());
+            //create div of btns    
+            const btnsDiv = document.createElement('div');
+            btnsDiv.classList.add('btns-div');
+            const cancelBtn = createCancelBtn();
+            btnsDiv.appendChild(cancelBtn);
+            btnsDiv.appendChild(createAddBtn());
+
+        form.appendChild(inputsDiv);                        
+        form.appendChild(btnsDiv);                        
+
+
+        //insert head div
+        dialog.appendChild(createHead('Add Task'));
+        dialog.appendChild(form);
+
+        document.body.appendChild(dialog);
+        dialog.showModal();
+
+        //add click event to close dialog
+        cancelBtn.addEventListener('click', () => dialog.close());
+    }
+
+    const addNewProjectDialog = () => {
+        const dialog = document.createElement('dialog');
+        const form = document.createElement('form');
+        form.setAttribute('method', 'dialog');
+
+            //create the div of inputs
+            const inputsDiv = document.createElement('div');
+            inputsDiv.classList.add('inputs-div');
+                inputsDiv.appendChild(createTitleInput())
+
+            //create div of btns    
+            const btnsDiv = document.createElement('div');
+            btnsDiv.classList.add('btns-div');
+            const cancelBtn = createCancelBtn();
+            btnsDiv.appendChild(cancelBtn);
+            btnsDiv.appendChild(createAddBtn());
+
+        form.appendChild(inputsDiv);                        
+        form.appendChild(btnsDiv);                        
+
+
+        //insert head div
+        dialog.appendChild(createHead('Add Project'));
+        dialog.appendChild(form);
+
+        document.body.appendChild(dialog);
+        dialog.showModal();   
+
+        //add click event to close dialog
+        cancelBtn.addEventListener('click', () => dialog.close());
+    }
+
+
+
+    return {
+        addNewProjectDialog,
+        addNewTodosDialog
+    }
+
+})(); 
+
+export default dialogBuilder ;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import "./style.css";
 import { mainHandler } from "./mainHandler.js";
 import { storage } from "./storageHandler.js";
 import { domHandler } from "./domHandler.js";
+import dialogBuilder from "./dialogBuilder.js";
 
 
-console.log('projects\n')
-domHandler.showProjects();
-domHandler.showTodosList('newNameValue')
+// console.log('projects\n')
+// domHandler.showProjects();
+// domHandler.showTodosList('newNameValue')
+
+
+//add click event to appear dialog of adding Project 
+const addNewProject = document.querySelector('#add-new-project-btn');
+addNewProject.addEventListener('click', () => {
+    dialogBuilder.addNewProjectDialog();
+})
+
+//add click event to appear dialog of adding Todos 
+const addNewTodos = document.querySelector('#add-new-todos-btn');
+addNewTodos.addEventListener('click', () => {
+    dialogBuilder.addNewTodosDialog();
+})
